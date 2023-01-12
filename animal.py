5e3c1b501eb73e7e7209bdf83f51e6b9413488d8
chansesyres <99367987+chansesyres@users.noreply.github.com>
2023-01-11 19:34:41 -0800

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Chanse E. Syres
+# GitHub username: chansesyres
+# Date: 01/11/2023
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
